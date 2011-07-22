fed9cd5f1fd6f57b9cbec5da362a3a5b97988775
Wlodek Bzyl <matwb@ug.edu.pl>
2011-07-22 22:37:22 +0200

    testing

---
+++ lib/doc/testing/BDD/accounts/features/step_definitions/account_steps.rb
@@ -0,0 +1,15 @@
+Given /^I have an account$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^it has a balance of (\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I take out (\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^my balance should be (\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
